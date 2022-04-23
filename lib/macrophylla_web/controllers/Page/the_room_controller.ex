c009b8485093c5b15e19a9f0236592abea3b7487
Kaikei-e <e_kaikei@protonmail.com>
2022-04-23 16:49:24 +0900

    Update:Searched in GitHUb code

--- lib/macrophylla_web/controllers/Page/the_room_controller.ex
+++ lib/macrophylla_web/controllers/Page/the_room_controller.ex
@@ -3,7 +3,8 @@ defmodule MacrophyllaWeb.TheRoomController do
 
   def show(conn, _params) do
     {:ok, list} = :inet.getif
-    IO.inspect Enum.at(list, 0)
+    #IO.inspect Enum.at(list, 0)
+    IO.inspect list
     render(conn, "the_room.html")
   end
 end
