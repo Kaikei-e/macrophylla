7daab2eccc6f8a7fdd1b1d245b31491828e02e27
Kaikei-e <e_kaikei@protonmail.com>
2022-04-28 02:52:27 +0900

    Update:

--- chat/lib/host/makeHost.exs
+++ chat/lib/host/makeHost.exs
@@ -1,6 +1,6 @@
 defmodule Host.MakeHost do
   def make_host do
-    prv_key = Base.encode16(:crypto.generate_key())
+    prv_key = Base.encode16(:crypto.generate_key(:rsa))
     IO.puts(prv_key)
   end
 end
