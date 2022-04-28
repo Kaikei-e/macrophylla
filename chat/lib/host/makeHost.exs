e9e18b692b45589529c9295bb77d1f4373ff5241
Kaikei-e <e_kaikei@protonmail.com>
2022-04-29 00:42:54 +0900

    Update: stucked

--- chat/lib/host/makeHost.exs
+++ chat/lib/host/makeHost.exs
@@ -1,6 +1,12 @@
 defmodule Host.MakeHost do
+  use Multiaddr
+
   def make_host do
-    prv_key = Base.encode16(:crypto.generate_key(:rsa, 2048))
+    {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048))
     IO.puts(prv_key)
+
+    maddr_string = "/ip4/127.0.0.1/tcp/80"
+    {:ok, maddr} = Multiaddr.new_multiaddr_from_string(maddr_string)
+    IO.puts(maddr)
   end
 end
--- chat/mix.exs
+++ chat/mix.exs
@@ -15,15 +15,14 @@ defmodule Chat.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      extra_applications: [:crypto],
+      extra_applications: [:crypto]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_multiaddr, "~> 0.1.0"}
     ]
   end
 end
