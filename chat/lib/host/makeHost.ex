c8e29fa0a2c13f272a1cbfc7ae35936a61cbebd7
Kaikei-e <e_kaikei@protonmail.com>
2022-04-29 20:08:34 +0900

    Update: learning about importing modules

--- chat/lib/host/makeHost.ex
+++ chat/lib/host/makeHost.ex
@@ -1,12 +1,12 @@
-defmodule Host.MakeHost do
-  use Multiaddr
+defmodule MakeHost do
+  alias Multiaddr, as: Ml_addr
 
   def make_host do
     {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048))
     IO.puts(prv_key)
 
     maddr_string = "/ip4/127.0.0.1/tcp/80"
-    {:ok, maddr} = Multiaddr.new_multiaddr_from_string(maddr_string)
+    {:ok, maddr} = Ml_addr.new_multiaddr_from_string(maddr_string)
     IO.puts(maddr)
   end
 end
