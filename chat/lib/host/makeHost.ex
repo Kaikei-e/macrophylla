a30a8cd3106d90edc2865f6caa64fa64338a02b0
Kaikei-e <e_kaikei@protonmail.com>
2022-04-30 23:49:45 +0900

    Update: understood ... I just declare the func but didn't exec...

--- chat/lib/host/makeHost.ex
+++ chat/lib/host/makeHost.ex
@@ -2,16 +2,19 @@ defmodule MakeHost do
   alias Multiaddr, as: Ml_addr
 
   def make_host(port \\ 80) do
-    # {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048))
+    {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048, :rsa_private))
 
-    maddr_string = "/ip4/127.0.0.1/tcp/" <> Integer.to_string(80)
+    # maddr_string = "/ip4/127.0.0.1/tcp/" <> Integer.to_string(port)
+    {:ok, maddr} =
+      Ml_addr.new_multiaddr_from_string("/ip4/127.0.0.1/tcp/" <> Integer.to_string(port))
+
+    # {:ok, maddr} = Ml_addr.new_multiaddr_from_string(maddr_string)
     # <> port
-    IO.puts("ip4")
 
     # maddr_string = "ip4" |> IO.inspect() |> IO.puts()
     # String.codepoints(:IO.puts(maddr_string))
     # s = String.graphemes(maddr_string)
 
-    # {:ok, maddr} = Ml_addr.new_multiaddr_from_string(maddr_string)
+    #
   end
 end
