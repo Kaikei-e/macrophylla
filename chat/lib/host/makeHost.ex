e365fa4919f925bbcfbbbc04a074e18ae23e8be1
Kaikei-e <e_kaikei@protonmail.com>
2022-04-30 23:38:11 +0900

    Update:

--- chat/lib/host/makeHost.ex
+++ chat/lib/host/makeHost.ex
@@ -4,9 +4,9 @@ defmodule MakeHost do
   def make_host(port \\ 80) do
     # {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048))
 
-    # maddr_string = "/ip4/127.0.0.1/tcp/" #<> port
+    maddr_string = "/ip4/127.0.0.1/tcp/" <> Integer.to_string(80)
     # <> port
-    IO.chardata_to_string(maddr_string = "ip4")
+    IO.puts("ip4")
 
     # maddr_string = "ip4" |> IO.inspect() |> IO.puts()
     # String.codepoints(:IO.puts(maddr_string))
