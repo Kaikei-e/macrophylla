40f08b8e53db959f6dc12fc4e2856ed68c8afb1a
Kaikei-e <e_kaikei@protonmail.com>
2022-04-30 22:33:29 +0900

    Update: I can't undeerstand how to print the var value like js or Go

--- chat/lib/host/makeHost.ex
+++ chat/lib/host/makeHost.ex
@@ -1,12 +1,13 @@
 defmodule MakeHost do
   alias Multiaddr, as: Ml_addr
 
-  def make_host do
+  def make_host(port \\ 80) do
     {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048))
-    IO.puts(prv_key)
 
-    maddr_string = "/ip4/127.0.0.1/tcp/80"
+    maddr_string = "/ip4/127.0.0.1/tcp/" <> port
+    # IO.puts(%{"the multi address is ": maddr_string})
+    to_string(maddr_string)
+
     {:ok, maddr} = Ml_addr.new_multiaddr_from_string(maddr_string)
-    IO.puts(maddr)
   end
 end
