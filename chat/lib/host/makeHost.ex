ba1d7e42fca3238fa93cae4c6ca74aa0d7e337e6
Kaikei-e <e_kaikei@protonmail.com>
2022-04-30 23:53:50 +0900

    Update: clean up

--- chat/lib/host/makeHost.ex
+++ chat/lib/host/makeHost.ex
@@ -4,17 +4,7 @@ defmodule MakeHost do
   def make_host(port \\ 80) do
     {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048, :rsa_private))
 
-    # maddr_string = "/ip4/127.0.0.1/tcp/" <> Integer.to_string(port)
-    {:ok, maddr} =
-      Ml_addr.new_multiaddr_from_string("/ip4/127.0.0.1/tcp/" <> Integer.to_string(port))
-
-    # {:ok, maddr} = Ml_addr.new_multiaddr_from_string(maddr_string)
-    # <> port
-
-    # maddr_string = "ip4" |> IO.inspect() |> IO.puts()
-    # String.codepoints(:IO.puts(maddr_string))
-    # s = String.graphemes(maddr_string)
-
-    #
+    maddr_string = "/ip4/127.0.0.1/tcp/" <> Integer.to_string(port)
+    {:ok, maddr} = Ml_addr.new_multiaddr_from_string(maddr_string)
   end
 end
