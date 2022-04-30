9e4e90b09d230d8b6937aa42d6c60dc7a0db223b
Kaikei-e <e_kaikei@protonmail.com>
2022-04-30 23:34:46 +0900

    Update: stuck

--- chat/lib/host/makeHost.ex
+++ chat/lib/host/makeHost.ex
@@ -2,12 +2,16 @@ defmodule MakeHost do
   alias Multiaddr, as: Ml_addr
 
   def make_host(port \\ 80) do
-    {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048))
+    # {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048))
 
-    maddr_string = "/ip4/127.0.0.1/tcp/" <> port
-    # IO.puts(%{"the multi address is ": maddr_string})
-    to_string(maddr_string)
+    # maddr_string = "/ip4/127.0.0.1/tcp/" #<> port
+    # <> port
+    IO.chardata_to_string(maddr_string = "ip4")
 
-    {:ok, maddr} = Ml_addr.new_multiaddr_from_string(maddr_string)
+    # maddr_string = "ip4" |> IO.inspect() |> IO.puts()
+    # String.codepoints(:IO.puts(maddr_string))
+    # s = String.graphemes(maddr_string)
+
+    # {:ok, maddr} = Ml_addr.new_multiaddr_from_string(maddr_string)
   end
 end
