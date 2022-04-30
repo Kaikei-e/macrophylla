b2d1ec383a68d26b37d61df95c8ca47ff491061b
Kaikei-e <e_kaikei@protonmail.com>
2022-05-01 00:10:54 +0900

    Update:

--- chat/lib/host/makeHost.ex
+++ chat/lib/host/makeHost.ex
@@ -2,7 +2,7 @@ defmodule MakeHost do
   alias Multiaddr, as: Ml_addr
 
   def make_host(port \\ 80) do
-    {_, prv_key} = Base.encode16(:crypto.generate_key(:rsa, 2048, :rsa_private))
+    {_, prv_key} = :crypto.generate_key(:rsa, 2048, 65537)
 
     maddr_string = "/ip4/127.0.0.1/tcp/" <> Integer.to_string(port)
     {:ok, maddr} = Ml_addr.new_multiaddr_from_string(maddr_string)
